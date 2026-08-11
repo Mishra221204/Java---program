869430c1e861b0d549a7e7ba426c946326961688
Shubham Mishra <mishrashubham76395@gmail.com>
2026-08-11 07:39:25 -0400

    Add files via upload
    Divide in number of Java

---
+++ Divide.java
@@ -0,0 +1,10 @@
+public class Divide {
+    
+    public static void main(String args[])
+    {
+      int a =50;
+      int b = 5;
+      int product=a/b;
+      System.out.println("divide the no"+product);
+    }
+}
